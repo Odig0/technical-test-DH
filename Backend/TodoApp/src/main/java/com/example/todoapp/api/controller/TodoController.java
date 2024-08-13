0ffcb6e804c7b3f3630863cff62dc11cb501d685
Diego <diegoguzman366@gmail.com>
2024-08-13 16:12:42 -0400

    Add Cors in Spring

--- Backend/TodoApp/src/main/java/com/example/todoapp/api/controller/TodoController.java
+++ Backend/TodoApp/src/main/java/com/example/todoapp/api/controller/TodoController.java
@@ -3,18 +3,13 @@ package com.example.todoapp.api.controller;
 import com.example.todoapp.api.model.Todo;
 import com.example.todoapp.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 public class TodoController {
 
     private final TodoService todoService;
